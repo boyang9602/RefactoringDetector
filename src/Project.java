a87131d4770e5d91bcebdd61fae70e6e839bfca7
Bo Yang <boyang9602@gmail.com>
2019-10-25 17:31:01 -0400

    change Project structure

--- src/Project.java
+++ src/Project.java
@@ -7,6 +7,12 @@ public class Project {
 	private String start;
 	private String end;
 	private FLAG_TYPE flagType;
+	private SCOPE scope;
+	
+	public enum SCOPE {
+		INTERVAL,
+		ALL
+	}
 	
 	public enum FLAG_TYPE {
 		TAG,
@@ -33,6 +39,10 @@ public class Project {
 		return flagType;
 	}
 	
+	public SCOPE getScope() {
+		return scope;
+	}
+	
 	public String getRepoAddr() {
 		return this.remoteAddr+ this.repoOwner + "/" + this.name + ".git";
 	}
@@ -42,15 +52,20 @@ public class Project {
 		jObj.put("remoteAddr", this.remoteAddr);
 		jObj.put("name", this.name);
 		jObj.put("repoOwner", this.repoOwner);
-		switch(flagType) {
-			case TAG:
-				jObj.put("startTag", this.start);
-				jObj.put("endTag", this.end);
-				break;
-			case COMMIT:
-				jObj.put("startCommit", this.start);
-				jObj.put("endCommit", this.end);
-				break;
+		if(this.scope == SCOPE.INTERVAL) {
+			switch(flagType) {
+				case TAG:
+					jObj.put("startTag", this.start);
+					jObj.put("endTag", this.end);
+					break;
+				case COMMIT:
+					jObj.put("startCommit", this.start);
+					jObj.put("endCommit", this.end);
+					break;
+			}
+			jObj.put("scope", "INTERVAL");
+		} else if(this.scope == SCOPE.ALL) {
+			jObj.put("scope", "ALL");
 		}
 		return jObj.toString();
 	}
@@ -61,5 +76,12 @@ public class Project {
 		this.start = start;
 		this.end = end;
 		this.flagType = flagType;
+		this.scope = SCOPE.ALL;
+	}
+	
+	public Project(String name, String repoOwner) {
+		this.name = name;
+		this.repoOwner = repoOwner;
+		this.scope = SCOPE.ALL;
 	}
 }
