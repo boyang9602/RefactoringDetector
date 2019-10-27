dd3316f43c7a48efefedda1657884777da46497a
Bo Yang <boyang9602@gmail.com>
2019-10-27 16:13:59 -0400

    update script

--- gen_mainfest.sh
+++ gen_mainfest.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
-rm manifests.md
+if [ -f manifests.md ]; then
+	rm manifests.md
+fi
 declare -A project_username=( ["hadoop"]="apache" ["hive"]="apache" ["accumulo"]="apache" ["kafka"]="apache" ["junit"]="junit-team" ["elasticsearch"]="elastic" ["fastjson"]="alibaba" ["logstash"]="elastic" ["jenkins"]="jenkinsci" ["spring-framework"]="spring-projects" )
 
+function gen_separate_line { 
+	local i=0;
+	local result='|';
+	for ((i=0;i<$1;i++)); do
+		result="${result} --- |"
+	done
+	echo $result;
+}
 base_dir="data/ref_infos"
-ref_types=$(ls $base_dir)
+ref_types=($(ls $base_dir))
 
 echo "# Summary" >> manifests.md
-for ref_type in $ref_types; do
-	echo "### $ref_type:" >> manifests.md
-	projects=$(ls $base_dir/$ref_type)
-	for project in $projects; do
-		files=($(ls $base_dir/$ref_type/$project))
-		echo "- $project: ${#files[@]}" >> manifests.md
+line="| Projects |"
+for ref_type in ${ref_types[*]}; do
+	line="${line} [${ref_type}](./${ref_type}_list.md) |"
+done
+echo $line >> manifests.md
+echo $(gen_separate_line ${#ref_types[@]}+1) >> manifests.md
+
+for project in ${!project_username[@]}; do
+	line="| ${project} |"
+	for ref_type in ${ref_types[*]}; do
+		if [ -d $base_dir/$ref_type/$project ]; then
+			files=($(ls $base_dir/$ref_type/$project))
+			line="${line} ${#files[@]} |"
+		else
+			line="${line} 0 |"
+		fi
 	done
+	echo $line >> manifests.md
 done
 
-echo "# List" >> manifests.md
-for ref_type in $ref_types; do
-	echo "### $ref_type" >> manifests.md
+for ref_type in ${ref_types[*]}; do
+	if [ -f "${ref_type}_list.md" ]; then
+		rm "${ref_type}_list.md"
+	fi
+	echo "### $ref_type" >> "${ref_type}_list.md"
 	projects=$(ls $base_dir/$ref_type/)
 	for project in $projects; do
-		echo "#### $project" >> manifests.md
+		echo "#### $project" >> "${ref_type}_list.md"
 		files=$(ls $base_dir/$ref_type/$project)
 		for file in $files; do
-			echo "##### [$file](./$base_dir/$ref_type/$project/$file):  " >> manifests.md
+			echo "##### [$file](./$base_dir/$ref_type/$project/$file):  " >> "${ref_type}_list.md"
 			commit_id=$(jq -r .commitId $base_dir/$ref_type/$project/$file)
 
-			echo "Commit: [$commit_id](https://github.com/${project_username[$project]}/$project/commit/$commit_id)" >> manifests.md
+			echo "Commit: [$commit_id](https://github.com/${project_username[$project]}/$project/commit/$commit_id)" >> "${ref_type}_list.md"
 		done
 	done
 done
